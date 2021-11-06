e2d94cfe0299fc1ef8425b9472bea79bf6eced38
hxckey <george.h@live.com>
2021-11-06 15:53:01 +0000

    add Title component for homepage

---
+++ src/Pages/home/Homepage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Homepage() {
+    return (
+        <>
+
+        <header>
+            MARIO KART RANDOMISER
+        </header>
+
+        </>
+    )
+}
