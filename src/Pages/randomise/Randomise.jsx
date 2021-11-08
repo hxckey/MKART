e9db2339ff17474e41c8d4e273441a88356de56c
hxckey <george.h@live.com>
2021-11-08 17:04:14 +0000

    fix errors with capitalisation

--- src/Pages/randomise/Randomise.jsx
+++ src/Pages/randomise/Randomise.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Header } from '../../layout/Header';
-import { Footer } from '../../layout/Footer';
+import { Header } from '../../layout/header';
+import { Footer } from '../../layout/footer';
 import { Display } from '../../components/DisplayBox';
 
 export function Randomiser() {
