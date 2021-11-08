f60f09e1bff64edee8cb644300ac36f39152fe76
hxckey <george.h@live.com>
2021-11-08 16:08:16 +0000

    remove duplicate displays from index.js Randomise

--- src/Pages/randomise/Randomise.jsx
+++ src/Pages/randomise/Randomise.jsx
@@ -1,16 +1,12 @@
 import React from 'react';
 import { Header } from '../../layout/Header';
 import { Footer } from '../../layout/Footer';
-import { Button } from '../../components/Button ';
 import { Display } from '../../components/DisplayBox';
 
 export function Randomiser() {
     return (
         <>
             <Header />
-            <Display />
-            <Display />
-            <Display />
             <Display />
             <Footer />
         </>
