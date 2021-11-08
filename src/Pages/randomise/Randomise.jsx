335a6f3b78c5113bf4011a09143467cf75eb3f4b
hxckey <george.h@live.com>
2021-11-08 15:42:11 +0000

    add display styling for radnomised items, create display and button components

--- src/Pages/randomise/Randomise.jsx
+++ src/Pages/randomise/Randomise.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { Header } from '../../layout/Header';
 import { Footer } from '../../layout/Footer';
+import { Button } from '../../components/Button ';
+import { Display } from '../../components/DisplayBox';
 
 export function Randomiser() {
     return (
         <>
             <Header />
+            <Display />
+            <Display />
+            <Display />
+            <Display />
             <Footer />
         </>
     )
