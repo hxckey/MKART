5f34abf4e66c6867e592881c09776278c4f7268b
hxckey <george.h@live.com>
2021-11-09 15:19:32 +0000

    add canvas to homepage and some basic styling

--- src/Pages/home/Homepage.jsx
+++ src/Pages/home/Homepage.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Title } from '../../components/Title';
 import { Footer } from '../../layout/footer';
+import { Canvas } from '../../components/Canvas';
 
 export function Homepage() {
     return (
         <>
             <Title />
+            <Canvas />
             <Footer />
         </>
     )
---
+++ src/components/Canvas/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './style.css';
+
+export function Canvas() {
+    return (
+        <>
+            <canvas></canvas>
+        </>
+    )
+}
