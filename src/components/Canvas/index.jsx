d1517881737406af0c48010649a9ba2ee57cbb50
hxckey <george.h@live.com>
2021-11-11 10:58:02 +0000

    add canvas functionality

--- src/components/Canvas/index.jsx
+++ src/components/Canvas/index.jsx
@@ -1,11 +1,69 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import './style.css';
 
-export function Canvas() {
+export function Canvas(props) {
+
+    const canvasRef = useRef(null)
+
+    useEffect(() => {
+        
+        const racetrack = canvasRef.current
+        const context = racetrack.getContext('2d') 
+
+
+        class Circle {
+            constructor(xpos, ypos, radius, color, speed) {
+                this.xpos = xpos;
+                this.ypos = ypos;
+                this.radius = radius;
+                this.color = color;
+                this.speed = speed;
+        
+                this.dx = 1 * this.speed;
+                this.dy = 1 * this.speed;
+            }
+        
+            draw(context) {
+                context.beginPath();
+                context.arc(this.xpos, this.ypos, this.radius, 0, Math.PI * 2, false)
+                context.stroke();
+            }
+        
+            update() {
+                context.clearRect(0, 0, 1000, 250)
+                this.draw(context);
+                
+                if ( (this.xpos + this.radius) > 750){
+                    this.dx = -this.dx;
+                }
+        
+                if ( (this.xpos - this.radius) < 0 ){
+                    this.dx = -this.dx
+                }
+                
+                this.xpos += this.dx
+        
+            }
+        }
+        
+        let circle1 = new Circle(26, 125, 25, "pink", 5);
+        circle1.draw(context);
+        
+        let updateCircle = function() {
+            requestAnimationFrame(updateCircle)
+            circle1.update();
+        }
+        
+        updateCircle();
+
+    }, [])
+
+
+
     return (
         <>
-            <canvas id="racetrack"></canvas>
-            {/* <script src="./script.js" ></script> */}
+            <canvas ref={canvasRef} {...props} id="racetrack"></canvas>
+
         </>
     )
 }
@@ -13,50 +71,4 @@ export function Canvas() {
 // let racetrack = document.getElementById("myCanvas");
 // const context = racetrack.getContext("2d");
 
-// // canvas.width = "1000px";
-// // canvas.height = "250px";
 
-// class Circle {
-//     constructor(xpos, ypos, radius, color, speed) {
-//         this.xpos = xpos;
-//         this.ypos = ypos;
-//         this.radius = radius;
-//         this.color = color;
-//         this.speed = speed;
-
-//         this.dx = 1 * this.speed;
-//         this.dy = 1 * this.speed;
-//     }
-
-//     draw(context) {
-//         context.beginPath();
-//         context.arc(this.xpos, this.ypos, this.radius, 0, Math.PI * 2, false)
-//         context.stroke();
-//     }
-
-//     update() {
-//         context.clearRect(0, 0, 1000, 250)
-//         this.draw(context);
-        
-//         if ( (this.xpos + this.radius) > 1000){
-//             this.dx = -this.dx;
-//         }
-
-//         if ( (this.xpos - this.radius) < 0 ){
-//             this.dx = -this.dx
-//         }
-        
-//         this.xpos += this.dx
-
-//     }
-// }
-
-// let circle1 = new Circle(26, 125, 25, "pink", 5);
-// circle1.draw(context);
-
-// let updateCircle = function() {
-//     requestAnimationFrame(updateCircle)
-//     circle1.update();
-// }
-
-// updateCircle();
