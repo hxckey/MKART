94305b06f9401abf56bc679daef82239b5886407
hxckey <george.h@live.com>
2021-11-11 10:31:10 +0000

    add comments to the dev webpack config

--- config/dev.webpack.config.js
+++ config/dev.webpack.config.js
@@ -1,8 +1,8 @@
 const config = require('./webpack.config.js');
 config.devServer = {
   historyApiFallback: true,
-  port: 8080,
-  liveReload: true,
+  port: 8080, //use this port for the server
+  liveReload: true, // refresh the browser when changes are saved
   open: true // opens the project in the browser when the server starts
 };
 
